598e25a0f9247bdedbe42f5d8dc96a1b57355540
Vishal Panchal <115503642+VishalPanchal11@users.noreply.github.com>
2024-05-10 01:30:53 +0530

    created components/ initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./Sections";
+
+const App = () => (
+  <main className="relative">
+    {/* <Nav/> */}
+    <section className="padding-b wide:padding-r xl:padding-l"><Hero/></section>
+    <section className="padding"><PopularProducts/></section>
+    <section className="padding"><SuperQuality/></section>
+    <section className="padding-x py-10"><Services/></section>
+    <section className="padding"><SpecialOffer/></section>
+    <section className="padding bg-pale-blue "><CustomerReviews/></section>
+    <section className="padding-x sm:py-32 py-16 w-full"><Subscribe/></section>
+    <section className="padding-x padding-t pb-8 bg-black"><Footer/></section>
+  </main>
+);
+
+export default App;
